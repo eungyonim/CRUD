a6b9e1a859b63a12d2bd460d2eeb2fcbf2f0c665
eungyonim <kry__@likelion.org>
2019-07-03 23:27:31 +0900

    NEW.html수정

---
+++ crud/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+import viewcrud.urls
+import viewcrud.views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', viewcrud.views.welcome, name="welcome"),
+    path('funccrud/', include(viewcrud.urls)),
+    # path('classcrud/', include(classcrud.urls)),
+]
